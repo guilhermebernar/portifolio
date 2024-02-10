892e666cf6a4ac93bf1ae84fae8f88b36496626d
guilhermebernar <guilhermebernardo@id.uff.br>
2024-02-10 18:23:36 -0300

    03 deploy resolution

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+const nextConfig = {
     trailingSlash: true,
     output: 'export',
     basePath: isProd ? '/portifolio' : '',
