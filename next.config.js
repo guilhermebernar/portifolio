20e2cace799c1634708a0695ed01955d9c1f9bc4
guilhermebernar <guilhermebernardo@id.uff.br>
2024-02-10 18:21:38 -0300

    02 deploy resolution

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
     trailingSlash: true,
     output: 'export',
+    basePath: isProd ? '/portifolio' : '',
+    assetPrefix: isProd ? '/portifolio/' : '',
 }
+
 
 module.exports = nextConfig
